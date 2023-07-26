bdb2968691739abbb248928be6007d719cf3d04b
plakas9 <plakas9@gmail.com>
2023-07-26 17:29:24 +0200

    wrote out a clear plan on how to tackle the problem in comments and tested that code was refelcting on browser

---
+++ RPS.js
@@ -0,0 +1,21 @@
+/*1. Create a function that randomly gets a choice from the computer  
+  
+ */
+
+
+
+// 2. create a function that takes two parameters and plays one round of rock paper scissors.
+// - first as input from computer
+// - second as input from player
+// - this function must declare a winner from that round, i.e. return a string declaring winner
+
+
+/*3.
+create a game() function.
+- play 5 rounds in a game.
+-keep a score
+-report a winner or loser in the end
+*/
+
+
+console.log("hello world");
