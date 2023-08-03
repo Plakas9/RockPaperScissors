eaeb6c66c7c596b51174f96025662e3fc8d1c7d3
plakas9 <plakas9@gmail.com>
2023-08-03 16:22:24 +0200

    added three buttons, Rock, Paper and scissors as well as event listeners to play  a round after each button is clicked.

--- RSP/RPS.js
+++ RSP/RPS.js
@@ -17,37 +17,6 @@ create a game() function.
 -report a winner or loser in the end
 */
 
-
-function getComputerChoice(){
-    const RSP_array = ["rock","paper","scissors"]; // RSP- short for rock, paper and scissors array 
-    // get random index value:
-    const randomIndex = Math.floor(Math.random()*RSP_array.length);//here we get a random index number
-    const CompChoice = RSP_array[randomIndex]; // will pick random item in array either R/S/P
-    return CompChoice;
-}
-
-
-
-
-
-function playRound(playerSelection = prompt("Rock, paper or scissors?"), computerSelection = getComputerChoice()){
-    
-    let localPlayerSelection = playerSelection.toLowerCase(); // this is to make game case-insensitive
-    
-    if(localPlayerSelection == computerSelection){ // case for draw
-        return "draw";
-      }else if(
-        (localPlayerSelection === "rock" && computerSelection === "scissors") || // All Cases where Player beats computer
-        (localPlayerSelection === "scissors" && computerSelection === "paper") ||
-        (localPlayerSelection === "paper" && computerSelection === "rock")
-      ){
-        return "Player Wins";
-    }else { // in any other case computer will win except if unknown string is entered. we'll deal with this later on.
-        return "Computer Wins"
-    }
-    
-}
-
 /*function FirstToFive(){ // This is my own function, it is different to the assignment in that it plays at most 100 games but if someone reaches 5 points then the game is over.
 
   let playerTracker = 0;
@@ -73,12 +42,59 @@ function playRound(playerSelection = prompt("Rock, paper or scissors?"), compute
 console.log(FirstToFive());
 */ 
 
+const Rbtn = document.querySelector('#R');
+const Pbtn = document.querySelector('#P');
+const Sbtn = document.querySelector('#S');
+
+
+Rbtn.addEventListener ('click',function(e){
+playRound("rock");
+});
+
+Pbtn.addEventListener('click',function(e){
+playRound("paper")
+});
+Sbtn.addEventListener('click', function(e){
+playRound("scissors");
+});
+
+
+function getComputerChoice(){  
+    const RSP_array = ["rock","paper","scissors"]; //  
+    // get random index value:
+    const randomIndex = Math.floor(Math.random()*RSP_array.length);//here we get a random index number
+    const CompChoice = RSP_array[randomIndex]; // will pick random item in array either R/S/P
+    return CompChoice;
+}
+
+
+function playRound(playerSelection , computerSelection = getComputerChoice()){
+    console.log(playerSelection);
+    let localPlayerSelection = playerSelection.toLowerCase(); // this is to make game case-insensitive
+    
+    if(localPlayerSelection == computerSelection){ 
+        return "draw";
+      }else if(
+        (localPlayerSelection === "rock" && computerSelection === "scissors") || // All Cases where Player beats computer
+        (localPlayerSelection === "scissors" && computerSelection === "paper") ||
+        (localPlayerSelection === "paper" && computerSelection === "rock")
+      ){
+        return "Player Wins";
+    }else { // in any other case computer will win except if unknown string is entered. 
+        return "Computer Wins"
+    }
+    
+
+}
+
+
+
 function game(){// this is a function without arrays.
   
   let message; //returned value of function stating who won/lost 
   let playerTracker = 0;
   let computerTracker=0;
-  for(let i = 0; i < 5; i++){
+  for(let i = 0; i < 6; i++){
     let round = playRound();
 
     if(round == "Computer Wins"){
@@ -97,11 +113,14 @@ function game(){// this is a function without arrays.
     }else if(playerTracker< computerTracker){
       message = "you lost the score was " + StringPlayerTracker + ":" + StringComputerTracker;
     }else{
-      message = "draw"
+      message = "draw" + StringPlayerTracker + ":" + StringComputerTracker;
     }
     return message;
 
 }
-console.log(game());
+//console.log(game());
+
+
+
 
 
